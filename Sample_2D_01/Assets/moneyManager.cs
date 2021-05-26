81b0d18ffd11999edcf524ab74c75b5e29e7e980
Junho5408 <57885238+Junho5408@users.noreply.github.com>
2021-05-26 17:57:36 +0900

    스테이지_생성, 프리펩_저장
    스테이지_생성, 프리펩_저장

--- Sample_2D_01/Assets/moneyManager.cs
+++ Sample_2D_01/Assets/moneyManager.cs
@@ -6,6 +6,7 @@ public class moneyManager : MonoBehaviour
 {
     static int money = 0;
     public GUISkin mySkin;
+    static int stageLevel = 0;
 
     public static void setMoney(int value)
     {
@@ -20,22 +21,19 @@ public class moneyManager : MonoBehaviour
 
     void OnGUI()
     {
-        //Make a background box
-        //GUI.Box(new Rect(10, 10, 202, 118), " Money : " + money.ToString());
-        //GUILayout.Label("Money : " + money.ToString());
 
-        // Assign the skin to be the one currently used.
         GUI.skin = mySkin;
 
         //Set the GUIStyle style to be label
         GUIStyle style = GUI.skin.GetStyle("label");
-        //Set the style font size to increase and decrease over time
         style.fontSize = (int)(50.0f);
 
         //Create a label and display with the current settings
         GUI.Label(new Rect(10, 10, 500, 200), "Money : "+money.ToString());
 
-        // Make a button. This will get the default "button" style from the skin assigned to mySkin.
-        //GUI.Button(new Rect(10, 10, 150, 20), "Skinned Button");
+
+        style.fontSize = (int)(50.0f);
+        GUI.Label(new Rect(10, 60, 500, 200), "STAGE " + stageLevel.ToString());
+
     }
 }
